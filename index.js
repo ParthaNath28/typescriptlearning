969fd4a4d1300a5b4225b86c4eecdcc8e260926f
Partha Nath <partha.cemk@gmail.com>
2019-07-21 15:04:44 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Person_1 = require("./Person");
-var foo = new Person_1.Person("Nath", "Partha");
-console.log(foo.getFullName());
